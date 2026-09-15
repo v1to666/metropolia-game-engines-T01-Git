458e514052757c156df6ef97bf55c84b166de574
alekaixd <aleksi.pirttinen@gmail.com>
2026-09-15 20:08:41 +0300

    Spawn cubes with space

--- Assets/Scripts/MeshGenerator.cs
+++ Assets/Scripts/MeshGenerator.cs
@@ -1,13 +1,24 @@
 using System.Collections;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class MeshGenerator : MonoBehaviour
 {
     [SerializeField] private Transform _prefab;
+    [SerializeField] private InputAction spawnAction;
 
     private void Start()
     {
+        spawnAction = InputSystem.actions.FindAction("Jump");
         StartCoroutine(Generate());
+    }
+
+    private void Update()
+    {
+        if (spawnAction.IsPressed())
+        {
+            Instantiate(_prefab);
+        }
     }
 
     private IEnumerator Generate()
